a5fca389c394ce9f9d1f6d5c7f8a34b123fd3e60
hacrun <Administrator@SS-PC>
2016-02-10 02:25:11 +0800

    change run.py

--- run.py
+++ run.py
@@ -1,26 +1,30 @@
 #coding:utf-8
 import os
 import os.path
+import sys
+
+device_id = sys.argv[1]
+pre_execute_str = "adb -s" + device_id
 
 #remove previous data
-os.system("adb shell rm -rf sdcard/report.xml")
+os.system(pre_execute_str +" shell rm -rf sdcard/report.xml")
 filename = 'report.xml'
 if os.path.exists(filename):
     os.remove(filename)
 
 #uninstall APP and test APP
-os.system("adb uninstall com.example.todolist")
-os.system("adb uninstall com.example.todolist.test")
+os.system(pre_execute_str + " uninstall com.example.todolist")
+os.system(pre_execute_str + " uninstall com.example.todolist.test")
 
 #build test APP
 os.system("ant clean debug")
 
 #install APP and test APP
-os.system("adb install ../todolist/bin/todolist-debug.apk")
-os.system("adb install bin/todolistTest-debug.apk")
+os.system(pre_execute_str + " install ../todolist/bin/todolist-debug.apk")
+os.system(pre_execute_str + " install bin/todolistTest-debug.apk")
 
 #start test
-os.system("adb shell am instrument -w -e reportDir sdcard -e reportFile report.xml com.example.todolist.test/.Runner.Runner1")
+os.system(pre_execute_str + " shell am instrument -w -e reportDir sdcard -e reportFile report.xml com.example.todolist.test/.Runner.Runner1")
 
 #pull test report
-os.system("adb pull sdcard/report.xml ./report.xml")
+os.system(pre_execute_str + " pull sdcard/report.xml ./report.xml")
