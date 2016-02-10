790ce61e491859e8d3350dfd3c2ea28c61ac20eb
hacrun <ma_0902@126.com>
2016-02-10 20:45:07 +0800

    test

--- src/com/example/todolist/test/testcase/login/LoginTestCase_crash_3.java
+++ src/com/example/todolist/test/testcase/login/LoginTestCase_crash_3.java
@@ -4,7 +4,7 @@ import com.example.todolist.test.test.BasicTestCase;
 
 public class LoginTestCase_crash_3 extends BasicTestCase{
 
-	public void Login4(){
+	public void testLogin4(){
 		android.os.Process.killProcess(android.os.Process.myPid());
 	}
 }
